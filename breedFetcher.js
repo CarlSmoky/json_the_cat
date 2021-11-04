cc4dbbcd45eddcf950c209ca007b854c9c45e3b2
tsumitakaoru <Katherine@KT-MacBook-Air-2.local>
2021-11-04 17:12:14 -0400

    add test case

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,26 @@
-const breed = process.argv.slice(2, 3);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-// const url = `https://api.thecapi.com/v1/breeds/search?q=${breed}`;
 const request = require('request');
 let data = '';
-request(url, (error, response, body) => {
-  if (error) {
-    return console.log('error:', error); // Print the error if one occurred
-  }
-  data = JSON.parse(body);
-  const breedData = data[0];
-  if (!breedData) {
-    const breedStr = breed.toString();
-    console.log(`${breedStr} cannot find in database`);
-  } else {
-    console.log(breedData.description);
-  }
-});
 
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    data = JSON.parse(body);
+    const breedData = data[0];
+
+    if (!breedData) {
+      callback('breed not found', null);
+      return;
+    }
+
+    const description = breedData.description;
+    callback(null, description);
+  });
+};
+
+
+module.exports = { fetchBreedDescription };
