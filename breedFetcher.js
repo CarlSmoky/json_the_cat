8d70d56aed4d19695af15e79c1bac542cd70933b
tsumitakaoru <Katherine@KT-MacBook-Air-2.local>
2021-11-04 16:02:34 -0400

    add error handling

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,19 @@
 const breed = process.argv.slice(2, 3);
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+// const url = `https://api.thecapi.com/v1/breeds/search?q=${breed}`;
 const request = require('request');
 let data = '';
-request(url, function (error, response, body) {
-  // console.error('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-  getDataFromJSON(body);
-});
-
-const getDataFromJSON = (body) => {
+request(url, (error, response, body) => {
+  if (error) {
+    return console.log('error:', error); // Print the error if one occurred
+  }
   data = JSON.parse(body);
-  if (data.length === 0) {
+  const breedData = data[0];
+  if (!breedData) {
     const breedStr = breed.toString();
     console.log(`${breedStr} cannot find in database`);
   } else {
-    console.log(data[0].description);
+    console.log(breedData.description);
   }
-};
+});
+
