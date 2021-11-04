e75b8b1744493544be22377b94281bbfcbb79bd6
tsumitakaoru <Katherine@KT-MacBook-Air-2.local>
2021-11-04 14:23:50 -0400

    add getDataFromJSON function

--- breedFetcher.js
+++ breedFetcher.js
@@ -7,13 +7,6 @@ request(url, function (error, response, body) {
   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Google homepage.
   getDataFromJSON(body);
-  // data = JSON.parse(body);
-  // if (data.length === 0) {
-  //   const breedStr = breed.toString();
-  //   console.log(`${breedStr} cannot find in database`);
-  // } else {
-  //   console.log(data[0].description);
-  // }
 });
 
 const getDataFromJSON = (body) => {
